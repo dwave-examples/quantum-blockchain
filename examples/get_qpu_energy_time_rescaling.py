Fixed a sign error in get_qpu_energy_time rescaling, added new embedding and updated solver naming info.

examples/get_qpu_energy_time_rescaling.py
@@ -199,7 +199,7 @@ def fit_rescaling_to_kibble_zurek_form(
         for seed_bqm in range(num_bqms)
     ]
     mean_residual_energy = np.mean(residual_energies)
-    if mean_residual_energy >= 0:
+    if mean_residual_energy <= 0:
         raise ValueError(
             "Expected residual energies should be positive, but the "
             f"expected value is {mean_residual_energy}."
@@ -254,7 +254,7 @@ def main(
         print(f"Solving for chip_id {qpu.properties['chip_id']}")
         print()
         print(
-            "A Kibble-Zurek model model provides a good description of the ensemble-average"
+            "A Kibble-Zurek model model provides a good description of the ensemble-average "
             "expected energy for all Advantage and Advantage2 processors given a suitable "
             "selection of the device and ensemble specific parameters. "
             "<Hp> = E_0 + E_1 (t_a/t_0)^{-kappa_t} (J/R_0)^{-kappa_R}. "